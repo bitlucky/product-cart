8f6a4e124e25a5dd8d5bffa9179ef19baa95cda0
bitlucky <lokeshtiwari877@gmail.com>
2019-07-11 13:48:47 +0530

    Updated

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.use('/add-product' , (req,res,next)=>{
+    console.log('in the middleware');
+    res.send('<h1> Welcome to the add-product page</h1>');
+});
 
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-
-app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+app.use('/' , (req,res,next)=>{
+    console.log('in another middleware');
+    res.send("<h1> Hello from Express</h1>");
 });
 
 app.listen(3000);
