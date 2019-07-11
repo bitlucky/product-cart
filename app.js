8fd5bf7b68b5508626c6d6bd84ed8da6bda9513b
bitlucky <lokeshtiwari877@gmail.com>
2019-07-11 18:52:10 +0530

    Styles Added

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+const path = require('path');
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use('/add-product' , (req,res,next)=>{
-    console.log('in the middleware');
-    res.send('<h1> Welcome to the add-product page</h1>');
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/' , (req,res,next)=>{
-    console.log('in another middleware');
-    res.send("<h1> Hello from Express</h1>");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin' , adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname , 'views' , '404.html'));
 });
 
 app.listen(3000);
