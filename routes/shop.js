86e0e2417047d5c295ba3a044a20b5be8114ba7a
bitlucky <lokeshtiwari877@gmail.com>
2019-07-11 20:20:41 +0530

    routes

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const express = require('express');
 
-
-
 const router = express.Router();
+const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname , '../' , 'views' , 'shop.html'));
+  console.log(adminData.products);
+  res.render('shop.pug');
 });
 
 module.exports = router;
