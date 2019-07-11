b70fa7b2251940ef7b87d6c31ff19bdbf2562f5b
bitlucky <lokeshtiwari877@gmail.com>
2019-07-11 21:49:26 +0530

    Routes Added

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,22 @@
 const path = require('path');
+
 const express = require('express');
 
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
-const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  res.render('shop.pug');
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
